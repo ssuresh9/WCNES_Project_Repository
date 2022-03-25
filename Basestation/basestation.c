0b3a1988b1d9113a6c9cd6111a8624354d670cad
ssuresh9 <90091853+ssuresh9@users.noreply.github.com>
2022-03-25 14:46:34 +0100

    vscode config

---
+++ Basestation/basestation.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include "contiki.h"
+#include "dev/leds.h"
+#include "net/netstack.h"
+#include "net/nullnet/nullnet.h"
+#include "nrf_radio.h"
+
+
+/* Declare our "main" process, the basestation_process */
+PROCESS(basestation_process, "Clicker basestation");
+/* The basestation process should be started automatically when
+ * the node has booted. */
+AUTOSTART_PROCESSES(&basestation_process);
+
+#define HDR_SIZE 5
+
+/* Holds the number of packets received. */
+static int count = 0;
+static int8_t last_rssi;
+
+
+static void print_packet(const void *data, uint8_t len) {
+    /* print sequence number, timestam, data (in hex) and RSSI of received data */
+    last_rssi = -(nrf_radio_rssi_sample_get());
+    printf("%u|%lu|%lu|", *((uint8_t *)data), *((uint32_t *)(data+1)), ((uint32_t)RTIMER_NOW())) ;
+    for (uint8_t i=0; i<len-HDR_SIZE; i++) {
+      printf("%02x ", *((uint8_t *)(data+i+HDR_SIZE)));
+    }
+    printf("|%d\n", last_rssi);
+
+}
+
+/* Callback function for received packets.
+ *
+ * Whenever this node receives a packet for its broadcast handle,
+ * this function will be called.
+ *
+ * As the client does not need to receive, the function does not do anything
+ */
+static void recv(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest) {
+    
+    count++;
+    /* 0bxxxxx allows us to write binary values */
+    /* for example, 0b10 is 2 */
+    leds_off(LEDS_ALL);
+    leds_on(count & 0b1111);
+    
+    print_packet(data, len);
+}
+
+/* Our main process. */
+PROCESS_THREAD(basestation_process, ev, data) {
+	PROCESS_BEGIN();
+
+    static struct etimer timer;
+	/* Initialize NullNet */
+	nullnet_set_input_callback(recv);
+
+    /* Setup a periodic timer that expires after 10 seconds. */
+    etimer_set(&timer, CLOCK_SECOND * 10);
+    
+    while (1) {
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+        etimer_restart(&timer);
+        /* print statistics */
+        // printf("\n--- statistics: received %d packets / 10s\n", count);
+        count = 0;
+    }
+    
+    
+	PROCESS_END();
+}
---
+++ Client/client.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include "contiki.h"
+#include "net/netstack.h"
+#include "net/nullnet/nullnet.h"
+
+/* Declare our "main" process, the client process*/
+PROCESS(client_process, "Clicker client");
+/* The client process should be started automatically when
+ * the node has booted. */
+AUTOSTART_PROCESSES(&client_process);
+
+#define FILE_SIZE 10000
+#define PKT_SIZE 32
+#define HDR_SIZE 5
+
+/* including a header to the packet:
+ * - 1B sequence number
+ * - 4B tx_timestamp
+ * 
+ * packet: buffer to be updated with the header
+ * seq: sequence number of the packet
+ */
+static void add_header(uint8_t *packet, uint8_t seq) {
+    static uint32_t timestamp = 0;
+    /* use first byte of the packet as sequence number. */
+    packet[0] = seq & 0xff;
+    /* four bytes of timestamp */
+    timestamp = (uint32_t)RTIMER_NOW();
+    memcpy(&packet[1], &timestamp, 4);
+}
+
+/* generaion of a random sequence.
+ * 
+ * packet: buffer to be filled with the random sequence
+ * seed: seed to initialize the random generator. 
+ *       (to have unique and reproducable sequences for every packet. 
+          Use the sequence number as seed!)
+ * length: length of the sequence
+ */
+static  void compute_sequence(uint8_t *packet, uint8_t seed, uint8_t length) {
+    const uint32_t A1 = 1664525;
+    const uint32_t C1 = 1013904223;
+    const uint32_t RAND_MAX1 = (((uint32_t)1 << 31) - 1);
+    const uint8_t MAX_BYTE = ((1<<8) - 1);      // one byte
+    uint8_t num = seed | (1<<4);                // seed (4 bites) is part of the seq number
+    for (uint8_t i=0; i<length; i++) {          // generate the random payload byte by byte
+        num = (num * A1 + C1) & RAND_MAX1;
+        packet[i] = (uint8_t)(num & MAX_BYTE);
+
+    }                 
+}
+
+
+/* Callback function for received packets.
+ *
+ * Whenever this node receives a packet for its broadcast handle,
+ * this function will be called.
+ *
+ * As the client does not need to receive, the function does not do anything
+ */
+static void send(const void* data, uint16_t len) {
+    memcpy(nullnet_buf, data, len);
+    nullnet_len = len;
+    NETSTACK_NETWORK.output(NULL);
+}
+
+static void recv(const void *data, uint16_t len,
+    const linkaddr_t *src, const linkaddr_t *dest) {
+}
+
+/* Our main process. */
+PROCESS_THREAD(client_process, ev, data) {
+    static uint8_t buffer[PKT_SIZE + HDR_SIZE];  // include 5 header bytes
+    static uint8_t counter = 0;
+    static struct etimer timer;
+
+	PROCESS_BEGIN();
+    
+	/* Initialize NullNet */
+	nullnet_buf = (uint8_t *)&buffer;
+	nullnet_len = sizeof(buffer);
+	nullnet_set_input_callback(recv);
+
+    /* Setup a periodic timer that expires after 2 seconds. */
+    etimer_set(&timer, CLOCK_SECOND / 1);
+    
+	/* Loop forever. */
+	while (1) {
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+        etimer_restart(&timer);
+
+        /* add header (3 bytes) */
+        add_header(&buffer[0], counter);
+        /* include random sequence as payload */
+        compute_sequence(&buffer[HDR_SIZE], counter, PKT_SIZE);
+        /* send data */
+        send(&buffer, sizeof(buffer));
+
+        counter++;
+    }
+
+	PROCESS_END();
+}
